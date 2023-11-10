26049f4e4ced58657574dc58519e09b847d4a0a1
Tayfun Sur <tayfunsur@gmail.com>
2023-11-10 14:30:55 +0300

    Exclude `manage`

--- src/constants.py
+++ src/constants.py
@@ -1,8 +1,8 @@
 # Removed user as it produces too many false positives, and likely wouldn't be related to anything malicious
 ROLE_CHANGE_KEYWORDS = [
-    'manage', 
-    'role', 
-    'admin', 
+    # 'manage',
+    'role',
+    'admin',
     'own',
     # 'root',
     # 'user',
